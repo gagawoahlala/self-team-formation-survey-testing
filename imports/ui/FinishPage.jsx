bb1a23bbe71454371dc862914de2d0e120416160
Jiashen Wang <jiashenwang@Jiashens-MacBook-Pro.local>
2017-04-21 13:30:59 -0700

    jw - bold the random number

--- imports/ui/FinishPage.jsx
+++ imports/ui/FinishPage.jsx
@@ -6,7 +6,7 @@ export default class FinishPage extends Component {
       <div className="finish-page">
         <h4>You're finished!</h4>
         <p>
-          Please copy this code ({this.props.code}) and place it into the text box in Mechanical Turk in order to get paid.  Thank you for your participation.
+          Please copy this code <b>({this.props.code})</b> and place it into the text box in Mechanical Turk in order to get paid.  Thank you for your participation.
         </p>
       </div>
     )
